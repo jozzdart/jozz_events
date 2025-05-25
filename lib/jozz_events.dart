ca45dbe169d79275fa81097f36f9b3ccfe7593db
jozz <yosefd99@gmail.com>
2025-05-26 01:31:39 +0300

    Lowered SDK constraint for wider compatibility
    - Lowered Dart SDK constraint to `>=2.12.0 <4.0.0` for wider compatibility

--- lib/jozz_events.dart
+++ lib/jozz_events.dart
@@ -3,6 +3,6 @@
 /// This package provides a lightweight, framework-agnostic event bus implementation
 /// that follows domain-driven design principles. It allows different components
 /// of an application to communicate without direct dependencies on each other.
-library;
+library jozz_events;
 
 export 'src/jozz_events.dart';
