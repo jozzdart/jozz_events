589b2d4e5d66a0e19c985fc3797f4a9ca083bbfc
jozz <yosefd99@gmail.com>
2025-05-26 01:25:40 +0300

    Update jozz_events.dart

--- lib/jozz_events.dart
+++ lib/jozz_events.dart
@@ -3,6 +3,6 @@
 /// This package provides a lightweight, framework-agnostic event bus implementation
 /// that follows domain-driven design principles. It allows different components
 /// of an application to communicate without direct dependencies on each other.
-library jozz_events;
+library;
 
 export 'src/jozz_events.dart';
